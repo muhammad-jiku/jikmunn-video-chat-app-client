79713a543720ac96d118068b308b3bea4f03f908
muhammad-jiku <muhammadjiku364@gmail.com>
2022-07-28 18:37:08 +0600

    messaging section is added

--- src/Components/CallPageContainer/Messenger/Messenger.jsx
+++ src/Components/CallPageContainer/Messenger/Messenger.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import './Messenger.scss';
+import {
+  FaCommentAlt,
+  FaPaperPlane,
+  FaTimes,
+  FaUserFriends,
+} from 'react-icons/fa';
 
 const Messenger = () => {
-  return <div>messages</div>;
+  return (
+    <div className="messengerContainer">
+      <div className="messengerHeader">
+        <h3>Meeting Details</h3>
+        <FaTimes className="icon" />
+      </div>
+      <div className="messengerHeaderTabs">
+        <div className="tab">
+          <FaUserFriends className="icon" />
+          <p>People (1)</p>
+        </div>
+
+        <div className="tab active">
+          <FaCommentAlt className="icon" />
+          <p>chat</p>
+        </div>
+      </div>
+
+      <div className="chatSection">
+        <div className="chatBlock">
+          <div className="sender">
+            You <small>12 PM</small>
+          </div>{' '}
+          <p className="msg">Here comes actual message</p>
+        </div>
+      </div>
+
+      <div className="sendMsgSection">
+        <input type="text" placeholder="Send a message to everyone" />
+        <FaPaperPlane className="icon" />
+      </div>
+    </div>
+  );
 };
 
 export default Messenger;
