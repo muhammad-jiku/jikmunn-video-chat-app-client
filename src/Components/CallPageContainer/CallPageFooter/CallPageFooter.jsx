f6a3f412a11b38e82a6c9c29224194e4a976b1cd
muhammad-jiku <muhammadjiku364@gmail.com>
2022-07-28 17:39:21 +0600

    footer for call section is added

--- src/Components/CallPageContainer/CallPageFooter/CallPageFooter.jsx
+++ src/Components/CallPageContainer/CallPageFooter/CallPageFooter.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import './CallPageFooter.scss';
+import {
+  FaAngleUp,
+  FaClosedCaptioning,
+  FaDesktop,
+  FaMicrophone,
+  FaPhone,
+  FaVideo,
+} from 'react-icons/fa';
 
 const CallPageFooter = () => {
-  return <div>hello</div>;
+  return (
+    <div className="footerItem">
+      <div className="leftItem">
+        <div className="iconBlock">
+          Meeting Details
+          <FaAngleUp className="icon" />
+        </div>
+      </div>
+      <div className="centerItem">
+        <div className="iconBlock">
+          <FaMicrophone className="icon" />
+        </div>{' '}
+        <div className="iconBlock">
+          <FaPhone className="icon red" />
+        </div>
+        <div className="iconBlock">
+          <FaVideo className="icon" />
+        </div>
+      </div>
+      <div className="rightItem">
+        <div className="iconBlock">
+          <FaClosedCaptioning className="icon red" />
+          <p className="title">Turn On Captions</p>
+        </div>
+        <div className="iconBlock">
+          <FaDesktop className="icon red" />
+          <p className="title">Present Now</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CallPageFooter;
