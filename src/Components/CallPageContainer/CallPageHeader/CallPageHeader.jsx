df0fb2a197c1cd8df34ab00977fc56a22626399d
muhammad-jiku <muhammadjiku364@gmail.com>
2022-07-28 13:59:02 +0600

    call page header is added

--- src/Components/CallPageContainer/CallPageHeader/CallPageHeader.jsx
+++ src/Components/CallPageContainer/CallPageHeader/CallPageHeader.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { HiUsers } from 'react-icons/hi';
+import { BsFillChatFill } from 'react-icons/bs';
+import { FaUserCircle } from 'react-icons/fa';
+import './CallPageHeader.scss';
 
 const CallPageHeader = () => {
   return (
-    <div>
-      <h1>hi</h1>
+    <div className="frameHeader">
+      <div className="headerItems iconBlock">
+        {' '}
+        <HiUsers className="icon" />
+      </div>
+      <div className="headerItems iconBlock">
+        {' '}
+        <BsFillChatFill className="icon" />
+        <span className="alertCircleIcon"></span>
+      </div>
+      <div className="headerItems timeBlock">
+        <span> 12:00 P.M.</span>
+      </div>
+      <div className="headerItems iconBlock">
+        {' '}
+        <FaUserCircle className="icon" />
+      </div>
     </div>
   );
 };
