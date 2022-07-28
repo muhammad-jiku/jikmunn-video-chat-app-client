1ffcb638cc99a44bfa47b0b85dda5fa3be3400e0
muhammad-jiku <muhammadjiku364@gmail.com>
2022-07-28 09:21:38 +0600

    home and 404 route and files added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import './App.scss';
+import NotFound from './Components/Shared/NotFound/NotFound';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello there!!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
