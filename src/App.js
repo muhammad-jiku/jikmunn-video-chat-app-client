ff34b72e5a502469e7fd73307e3df950e734efc1
muhammad-jiku <muhammadjiku364@gmail.com>
2022-07-28 09:30:06 +0600

    header is added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.scss';
+import Header from './Components/Shared/Header/Header';
 import NotFound from './Components/Shared/NotFound/NotFound';
 import CallPage from './Pages/CallPage/CallPage';
 import Home from './Pages/Home/Home';
@@ -8,6 +9,7 @@ import Home from './Pages/Home/Home';
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:id" element={<CallPage />} />
