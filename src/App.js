1b8cb988563b7335ac40aad21cb2b4c871c02490
muhammad-jiku <muhammadjiku364@gmail.com>
2022-07-29 12:41:09 +0600

    react app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import './App.scss';
-// import Header from './Components/Shared/Header/Header';
-import NotFound from './Components/Shared/NotFound/NotFound';
-import CallPage from './Pages/CallPage/CallPage';
-import Home from './Pages/Home/Home';
+import './App.css';
 
 function App() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:id" element={<CallPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
-  );
+  return <div>hello</div>;
 }
 
 export default App;
