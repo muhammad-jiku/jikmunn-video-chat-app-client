a461d1bca16ada2f6955480c923a61744d817ec9
muhammad-jiku <muhammadjiku364@gmail.com>
2022-08-19 12:04:44 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Notifications from './components/Notifications';
-import Options from './components/Options';
-import VideoPlayer from './components/VideoPlayer';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <VideoPlayer />
-      <Options>
-        {' '}
-        <Notifications />{' '}
-      </Options>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
